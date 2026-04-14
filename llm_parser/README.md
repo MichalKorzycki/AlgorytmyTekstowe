a8904c6a33ad93ebc5b1e01f5565727fc3be1724
Michał Korzycki <michal.korzycki@gmail.com>
2026-04-14 08:41:44 +0200

    (feat) LLM Parser

---
+++ llm_parser/README.md
@@ -0,0 +1,12 @@
+# Parsowanie HTML-a LLM-em
+
+## Instalacja:
+- Zainstalować środowisko z `requirements.txt`
+- Zainstalować [Ollama](https://ollama.com/) 
+- Uruchomić: 
+  - `ollama create Gemma_html_parser -f Gemma_html_parser.model`
+  - `ollama create Gemma_html_parser_large_ctx -f Gemma_html_parser_large_ctx.model`
+
+
+
+
---
+++ llm_parser/llm_parser3.py
@@ -0,0 +1,81 @@
+import ollama
+import sys
+import re
+import json
+import time
+from json_repair import repair_json
+from pprint import pprint
+
+from html_utils import clean_html
+
+
+def create_prompt(
+    text: str,  schema: str = None,
+) -> str:
+    """
+    Create a prompt for the model with optional instruction and JSON schema.
+    """
+    schema = ""
+    instruction = """
+    Extract the specified information for all apartment ads and present it in a structured JSON format. 
+    One ad contains the information about the price is expressed in zł. 
+    The area (powierzchnia) is expressed in square meters m² NEVER as zł/m².
+    The number of rooms (pokoje) is an int. 
+    The floor (piętro) is an int.
+    Price of squareMeter is expressed in  zł/m² 
+    """
+    prompt = f"{instruction}\n```html\n{text}\n```\nThe JSON schema is as follows:```json\n{schema}\n```"
+
+    messages = [
+        {
+            "role": "user",
+            "content": prompt,
+        }
+    ]
+
+    return str(messages)
+
+
+schema = """
+{
+  "type": "object",
+  "properties": {
+    "price": {
+      "type": "str"
+    },
+    "area": {
+      "type": "str"
+    },
+    "rooms": {
+      "type": "int"
+    },
+    "floor": {
+      "type": "int"
+    },
+    "pricePerSquareMeter" : {
+        "type": "int"
+    }
+  },
+  "required": ["price", "area", "rooms", "floor", "pricePerSquareMeter"],
+}
+"""
+
+from ollama_python.endpoints import GenerateAPI
+
+# https://www.morizon.pl/oferta/sprzedaz-mieszkanie-warszawa-wlochy-plastyczna-31m2-mzn2046023842
+if __name__ == '__main__':
+    n = int(sys.argv[2]) if len(sys.argv) > 2 else 1
+    with open(sys.argv[1]) as f:
+        html = f.read()
+        html = clean_html(html)
+        input_prompt = create_prompt(str(html),  schema=schema)
+        for i in range(n):
+            start = time.time()
+            response = ollama.generate(prompt=input_prompt, model='Gemma_html_parser_large_ctx',
+                                       format="json",
+                                       keep_alive="5m")['response']
+            good_json_string = repair_json(response)
+            end = time.time()
+            print(f'Response {i} time: {end - start:.2f} sec')
+            pprint(json.loads(good_json_string))
+            print()
